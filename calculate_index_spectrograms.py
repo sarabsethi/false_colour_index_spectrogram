357eadd193723014e3a745943fefd4efbb4364f1
sarabsethi <sarab.sethi93@gmail.com>
2018-02-01 17:41:25 +0000

    add option to save figures to file. For example: -savef=output.png

--- calculate_index_spectrograms.py
+++ calculate_index_spectrograms.py
@@ -132,26 +132,28 @@ def plot_fci_spectrogram(data_dir, doscaling=True, Fs=44100):
 
 	matplotlib.rcParams.update({'font.size': 30})
 	matplotlib.rcParams.update({'font.family' : 'serif'})
-	fig=plt.figure(facecolor='white')
+	fig=plt.figure(figsize=(15, 5), facecolor='white')
 	plt.imshow(false_colour_image, aspect='auto', origin='lower', interpolation='none', extent=(0, maxtime, 0, Fs/2))
 	plt.xlabel('Time (%s)' % timeunits)
 	plt.ylabel('Frequency (Hz)')
+	plt.tight_layout()
 	return plt.gcf()
 
 ########################
 if __name__=='__main__':
 	import argparse
 
-	default_in  = '/home/dans/birdsong/bl_dawnchorus_atmospheres/as_mono/022A-WA09020XXXXX-0916M0.flac'
-	#default_in  = 'input_audio'
-	default_in  = '/home/dans/birdsong/jolle/20120203-AU1.WAV'
-	default_out = 'output_spectrograms'
+	#default_in  = '/home/dans/birdsong/bl_dawnchorus_atmospheres/as_mono/022A-WA09020XXXXX-0916M0.flac'
+	default_in  = 'input_audio'
+	#default_in  = '/home/dans/birdsong/jolle/20120203-AU1.WAV'
+	default_out = 'output_spectrograms_1_week_from_16_nov_17_e100'
 
 	parser = argparse.ArgumentParser()
 	parser.add_argument("inpaths", nargs='*', default=default_in, help="Input path: can be a path to a single file (which will be chunked), or a folder full of wavs, or the input can be a list of wav files which you explicitly specify")
 	parser.add_argument("-o", default=default_out, type=str, help="Output path: a folder (which should exist already) in which data files will be written.")
 	parser.add_argument("-c", default=1, type=int, choices=[0,1], help="Whether to calculate the stats afresh. Use -c=0 to reuse previously calculated stats.")
 	parser.add_argument("-n", default=0, type=int, choices=[0,1], help="Whether to apply scaling (normalisation) of the statistics before plotting them.")
+	parser.add_argument("-savef", default='', type=str, help="Image file where the output figure should be saved (including extension (png, jpg, etc.). Expect issues with vector graphics")
 	args = parser.parse_args()
 	print args
 
@@ -160,5 +162,7 @@ if __name__=='__main__':
 
 	# now plot
 	ourplot = plot_fci_spectrogram(args.o, doscaling=args.n)
+	if not args.savef == "":
+		ourplot.savefig(args.savef)
 	ourplot.show()
 	raw_input("Press a key to close")
